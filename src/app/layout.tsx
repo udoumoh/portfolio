4a87e19765d785c5653fd49d0f3a3686639c7f31
aurora <johnudoumoh63@gmail.com>
2025-03-31 10:00:14 +0100

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Nav from "@/components/Nav/nav";
 import Footer from "@/components/Footer/footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -31,31 +32,61 @@ export default function RootLayout({
       <head>
         <title>John Udoumoh | Frontend Engineer & Next.js Developer</title>
         <link rel="icon" href="/assets/portfolio-logo.svg" />
-        <meta name="description" content="Hi, I'm John Udoumoh, a Frontend Engineer specializing in Next.js, React, and TypeScript. Explore my portfolio to see my latest projects and expertise."/>
-        <meta name="keywords" content="Frontend Developer, Next.js Developer, React Engineer, Software Engineer, Portfolio, Web Development"/>
-        <meta name="author" content="John Udoumoh"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <meta name="robots" content="index, follow"/>
+        <meta
+          name="description"
+          content="Hi, I'm John Udoumoh, a Frontend Engineer specializing in Next.js, React, and TypeScript. Explore my portfolio to see my latest projects and expertise."
+        />
+        <meta
+          name="keywords"
+          content="Frontend Developer, Next.js Developer, React Engineer, Software Engineer, Portfolio, Web Development"
+        />
+        <meta name="author" content="John Udoumoh" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
       </head>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-VQHPFFMBM8"
+      ></script>
+      {/* Load Google Analytics script */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-VQHPFFMBM8"
+      />
+
+      {/* Initialize Google Analytics */}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-VQHPFFMBM8');
+        `}
+      </Script>
+
       <body className={`${inter.variable} ${spaceMono.variable} antialiased`}>
         <SpeedInsights />
         <Nav />
         {children}
         <Footer />
       </body>
-      <script type="application/ld+json" dangerouslySetInnerHTML={{
-        __html: JSON.stringify({
-          "@context": "https://schema.org",
-          "@type": "Person",
-          "name": "John Udoumoh",
-          "jobTitle": "Frontend Engineer",
-          "url": "https://yourwebsite.com",
-          "sameAs": [
-            "https://github.com/udoumoh",
-            "https://www.linkedin.com/in/johnudoumoh"
-          ]
-        })
-      }} />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Person",
+            name: "John Udoumoh",
+            jobTitle: "Frontend Engineer",
+            url: "https://yourwebsite.com",
+            sameAs: [
+              "https://github.com/udoumoh",
+              "https://www.linkedin.com/in/johnudoumoh",
+            ],
+          }),
+        }}
+      />
     </html>
   );
 }
